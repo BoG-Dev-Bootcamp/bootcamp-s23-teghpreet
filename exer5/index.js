25485a4392576da100aa39975471cf45122f7095
afazio1 <alexa.fazio04@gmail.com>
2023-03-01 10:01:04 -0500

    fix index.js async problem

--- exer5/index.js
+++ exer5/index.js
@@ -1,14 +1,14 @@
 import { connectDB, closeDB } from "./utils/db.js"
 import { readDog, readDogs, createDog, updateDog, deleteDog } from "./operations.js"
 
-connectDB() // function that creates a connection to your db
+await connectDB() // function that creates a connection to your db
 
 // tests - feel free to change these or add more
-const test1 = createDog({ name: "Casey", age: 10, breed: "Pomeranian", dateEnter: Date.now() })
-const test2 = readDog({ name: "Casey" })
-const test3 = readDogs()
-const test4 = updateDog({ name: "Casey"}, {age: 11})
-const test5 = deleteDog({ name: "Casey" })
+const test1 = await createDog({ name: "Casey", age: 10, breed: "Pomeranian", dateEnter: Date.now() })
+const test2 = await readDog({ name: "Casey" })
+const test3 = await readDogs()
+const test4 = await updateDog({ name: "Casey"}, {age: 11})
+const test5 = await deleteDog({ name: "Casey" })
 
 
 console.log(test1)
@@ -17,5 +17,5 @@ console.log(test3)
 console.log(test4)
 console.log(test5)
 
-closeDB() // function that closes the connection to your db
+await closeDB() // function that closes the connection to your db
 
